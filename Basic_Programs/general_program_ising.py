dca47bd1352e66afe6712d0009afb6f99d9d393d
tomsmierz <smierzchalski.tomek@gmail.com>
2022-01-22 20:59:16 +0000

    experiments

--- Basic_Programs/general_program_ising.py
+++ Basic_Programs/general_program_ising.py
@@ -21,19 +21,34 @@
 
 # Import the functions and packages that are used
 from dwave.system import EmbeddingComposite, DWaveSampler
+import dwave_networkx as dnx
+from numpy.random import default_rng
+import pandas as pd
 
-# Define the problem as two Python dictionaries:
-#   h for linear terms, J for quadratic terms
-h = {}
-J = {('A','K'): -0.5,
-    ('B','C'): -0.5, 
-    ('A','C'): 0.5}
+rng = default_rng()
+NUM_READS = 10
 
-# Define the sampler that will be used to run the problem
-sampler = EmbeddingComposite(DWaveSampler())
 
-# Run the problem on the sampler and print the results
-sampleset = sampler.sample_ising(h, J,
-                                 num_reads = 10,
-                                 label='Example - Simple Ocean Programs: Ising')
-print(sampleset)
+
+for i in range(1,11):
+    graph = dnx.chimera_graph(16, 16, 4)
+    edge_attr = {edge: rng.normal(0, 1) for edge in graph.edges}
+    external = {node: rng.normal(0, 1) for node in graph.nodes}
+
+
+    # Define the sampler that will be used to run the problem
+    sampler = EmbeddingComposite(DWaveSampler())
+
+    # Run the problem on the sampler and print the results
+    sampleset = sampler.sample_ising(external, edge_attr,
+                                    num_reads = NUM_READS,
+                                    label='Example - Simple Ocean Programs: Ising')
+
+    df =sampleset.to_pandas_dataframe(sample_column = True)
+
+    df["h"] = [external for _ in range(NUM_READS)]
+    df["J"] = [edge_attr for _ in range(NUM_READS)]
+    print(i)
+    df.to_csv(f"data/128_{i}.csv")
+
+
